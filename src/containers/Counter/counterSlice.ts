bd53322bd00d4f9ff8a6a6e3dcea3ea1630cc3ef
cashrina <khripunova.irina.24@gmail.com>
2024-07-12 00:38:36 +0600

    Добавлена функция ограничения символов

--- src/containers/Counter/counterSlice.ts
+++ src/containers/Counter/counterSlice.ts
@@ -3,11 +3,13 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 interface CounterState {
     value: string;
     color: string;
+    isInputComplete: boolean;
 }
 
 const initialState: CounterState = {
     value: '',
-    color: ''
+    color: '',
+    isInputComplete: false,
 }
 
 const correctPassword = '1337';
@@ -16,8 +18,13 @@ export const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
-        increase: (state,action: PayloadAction<number>) => {
-            state.value += action.payload;
+        increase: (state, action: PayloadAction<number>) => {
+            if (state.value.length < 4) {
+                state.value += action.payload;
+                if (state.value.length === 4) {
+                    state.isInputComplete = true;
+                }
+            }
         },
         decrease: (state) => {
             state.value = state.value.slice(0, -1);
