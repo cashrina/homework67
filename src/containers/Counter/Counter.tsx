ebcf296dec0a2111382feb5531f81771f334d2b6
cashrina <khripunova.irina.24@gmail.com>
2024-07-12 15:38:00 +0600

    Прописан общий вид приложения

--- src/containers/Counter/Counter.tsx
+++ src/containers/Counter/Counter.tsx
@@ -9,20 +9,82 @@ const Counter = () => {
     const dispatch = useDispatch();
 
     return (
-        <div className="container mt-2">
-            <h1 className={color}>{displayValue}</h1>
-            <button onClick={() => dispatch(increase(7))} disabled={isInputComplete}>7</button>
-            <button onClick={() => dispatch(increase(8))} disabled={isInputComplete}>8</button>
-            <button onClick={() => dispatch(increase(9))} disabled={isInputComplete}>9</button>
-            <button onClick={() => dispatch(increase(4))} disabled={isInputComplete}>4</button>
-            <button onClick={() => dispatch(increase(5))} disabled={isInputComplete}>5</button>
-            <button onClick={() => dispatch(increase(6))} disabled={isInputComplete}>6</button>
-            <button onClick={() => dispatch(increase(1))} disabled={isInputComplete}>1</button>
-            <button onClick={() => dispatch(increase(2))} disabled={isInputComplete}>2</button>
-            <button onClick={() => dispatch(increase(3))} disabled={isInputComplete}>3</button>
-            <button onClick={() => dispatch(decrease())} disabled={!displayValue}>C</button>
-            <button onClick={() => dispatch(increase(0))} disabled={isInputComplete}>0</button>
-            <button onClick={() => dispatch(check())} disabled={!isInputComplete}>E</button>
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-4 border rounded p-4 shadow">
+                    <h1 className={`text-center ${color}`}>{displayValue}</h1>
+                    <div className="d-grid gap-2 mt-4">
+                        <div className="row ms-4">
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(7))}
+                                        disabled={isInputComplete}>7
+                                </button>
+                            </div>
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(8))}
+                                        disabled={isInputComplete}>8
+                                </button>
+                            </div>
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(9))}
+                                        disabled={isInputComplete}>9
+                                </button>
+                            </div>
+                        </div>
+                        <div className="row ms-4">
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(4))}
+                                        disabled={isInputComplete}>4
+                                </button>
+                            </div>
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(5))}
+                                        disabled={isInputComplete}>5
+                                </button>
+                            </div>
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(6))}
+                                        disabled={isInputComplete}>6
+                                </button>
+                            </div>
+                        </div>
+                        <div className="row ms-4">
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(1))}
+                                        disabled={isInputComplete}>1
+                                </button>
+                            </div>
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(2))}
+                                        disabled={isInputComplete}>2
+                                </button>
+                            </div>
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(3))}
+                                        disabled={isInputComplete}>3
+                                </button>
+                            </div>
+                        </div>
+                        <div className="row ms-4">
+                            <div className="col">
+                                <button className="btn btn-danger" onClick={() => dispatch(decrease())}
+                                        disabled={!displayValue}>C
+                                </button>
+                            </div>
+                            <div className="col">
+                                <button className="btn btn-light" onClick={() => dispatch(increase(0))}
+                                        disabled={isInputComplete}>0
+                                </button>
+                            </div>
+                            <div className="col">
+                                <button className="btn btn-success" onClick={() => dispatch(check())}
+                                        disabled={!isInputComplete}>E
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
